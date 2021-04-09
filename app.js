65d6ce208c1baf4f1e0e78b1ee4f7f66bd459162
chunzg <chunzhang53@gmail.com>
2021-04-09 20:24:16 +0100

    Fix black keys to make them return to black on keyup

--- app.js
+++ app.js
@@ -1,53 +1,33 @@
 const key = document.querySelectorAll('.key')
-// const secondColour = document.querySelector(".second-colour");
 
 window.addEventListener('keydown', function(e) {
   const audio = document.querySelector(`audio[data-key='${e.keyCode}']`)
     const key = document.querySelector(`.key[data-key='${e.keyCode}']`)
-    // const note = document.querySelector(".white-key")
     if(!audio) return; //stop function running altogether
     audio.currentTime = 0; //rewind to the start
     audio.play();
-
-    // if (e.keyCode === 65 || e.keyCode === 83 || e.keyCode === 68 || 
-    //     e.keyCode === 70 || e.keyCode === 71 || e.keyCode === 72 || e.keyCode === 74 || e.keyCode === 75 ) {
-    // key.style.backgroundColor = "blue";
-    // }
-
-    // key.classList.toggle("secondColour");
-    console.log(audio.currentTime);
+    key.style.backgroundColor = "blue";
     key.classList.add('playing');
-    // key.forEach(k => {
-    //   k.style.backgroundColor = red;
-    // })
-      // Trying to stop the player if it goes above 1 second
-     
+})
 
-  })
-
-
-
-  window.addEventListener('keyup',function(e){
-    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`)
-    const key2 = document.querySelector(`.key[data-key='${e.keyCode}']`)
-
-    setInterval(function(){
-      if (audio.currentTime > 1) {
-          audio.pause();
-          // key.style.backgroundColor = "white"
-
-         
-          
-      }
-    }, 0);
-
-    // if  (key.style.backgroundColor = "blue") {
-    //   key.style.backgroundColor = "white"
-    // }
-
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    key.classList.remove('playing');
-  });
+window.addEventListener('keyup',function(e){
+  const audio = document.querySelector(`audio[data-key='${e.keyCode}']`)
+  const key = document.querySelector(`.key[data-key='${e.keyCode}']`)
+  // Stop the sound if it goes above 1 second
+  setInterval(function(){
+    if (audio.currentTime > 1) {
+        audio.pause();
+        if(e.keyCode === 87 || e.keyCode === 69 || 
+          e.keyCode === 84 || e.keyCode === 89 || 
+          e.keyCode === 85) {
+            key.style.backgroundColor = "black"
+        } else {
+          key.style.backgroundColor = "white"
+        }
+    }
+  }, 0);
+  key.classList.remove('playing');
+});
 
   
 
