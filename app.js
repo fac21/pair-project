8675b65d67b90274cde5ba255f510717503734b0
mikailbadoula <michaeldevfay@gmail.com>
2021-04-09 14:35:06 +0100

    Add sounds

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const key = document.querySelectorAll('.key')
 
 window.addEventListener('keydown', function(e) {
+  console.log('hi');
     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`)
     const key = document.querySelector(`.key[data-key='${e.keyCode}']`)
     if(!audio) return; //stop function running altogether
     audio.currentTime = 0; //rewind to the start
-    audio.play()
-    key.classList.add('playing')
+    
+    audio.play();
+    console.log(audio.currentTime);
+    key.classList.add('playing');
+      // Trying to stop the player if it goes above 1 second
+     
+
   })
 
-  keys.forEach(x => x.addEventListener('transitionend', () => {
-    x.classList.remove('playing')
-  }))
+  window.addEventListener('keyup',function(e){
+    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`)
+    setInterval(function(){
+      if (audio.currentTime > 1) {
+          audio.pause();
+      }
+    }, 0);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    key.classList.remove('playing');
+  });
 
-  
+
+
