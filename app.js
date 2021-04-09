c617cd3bb31bd305dccccb4346f089116c6f3da1
chunzg <chunzhang53@gmail.com>
2021-04-09 22:58:41 +0100

    Amend setTimeout function

--- app.js
+++ app.js
@@ -8,24 +8,25 @@ window.addEventListener('keydown', function(e) {
     audio.play();
     key.style.backgroundColor = "blue";
     key.classList.add('playing');
+
+  //When keydown, wait 1s (sound keeps playing during this time), then stop sound
+  setTimeout(function(){
+    audio.pause();
+    if(e.keyCode === 87 || e.keyCode === 69 || 
+      e.keyCode === 84 || e.keyCode === 89 || 
+      e.keyCode === 85) {
+        key.style.backgroundColor = "black"
+    } else {
+      key.style.backgroundColor = "white"
+    }
+  }, 1000);
+
 })
 
 window.addEventListener('keyup',function(e){
-  const audio = document.querySelector(`audio[data-key='${e.keyCode}']`)
-  const key = document.querySelector(`.key[data-key='${e.keyCode}']`)
-  // Stop the sound if it goes above 1 second
-  setInterval(function(){
-    if (audio.currentTime > 1) {
-        audio.pause();
-        if(e.keyCode === 87 || e.keyCode === 69 || 
-          e.keyCode === 84 || e.keyCode === 89 || 
-          e.keyCode === 85) {
-            key.style.backgroundColor = "black"
-        } else {
-          key.style.backgroundColor = "white"
-        }
-    }
-  }, 0);
+  // const audio = document.querySelector(`audio[data-key='${e.keyCode}']`)
+  // const key = document.querySelector(`.key[data-key='${e.keyCode}']`)
+
   key.classList.remove('playing');
 });
 
